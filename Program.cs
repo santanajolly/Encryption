7491b62bf437b7ced813207f9d3e3d8634f13962
Santana Jolly <santanaharris@gmail.com>
2019-09-24 09:36:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //build a menu
+            //ability to create accounts
+            //four classes
+            //base class, properties like username password
+            //create
+            //establish account
+        }
+    }
+}
